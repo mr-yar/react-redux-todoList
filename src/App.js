13623eb988712e3a57d6f0d5a2eea15719be937d
mr-yar <59079474+mr-yar@users.noreply.github.com>
2021-02-05 13:23:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">To do List</h1>
+      <h2 className="subtitle">
+        Вы можете создавать задачи, выделять важное и сортировать по
+        параметрам или вручную
+      </h2>
+      <ul className="list" />
+      <label htmlFor="list-checkbox" className="list-toggle">
+        <span className="list-toggle__text">
+          Переместить выполненные задачи вниз?
+        </span>
+        <input
+          type="checkbox"
+          name="list-checkbox"
+          className="list-toggle__checkbox"
+        />
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label
+          htmlFor="list-checkbox"
+          className="list-toggle__label"
+        />
+      </label>
+      <div className="adding">
+        <input
+          type="text"
+          className="input"
+          placeholder="Введите задачу"
+        />
+        <button className="add-elem-btn">
+          <span className="material-icons"> add </span>
+        </button>
+      </div>
+
+      <div className="nav">
+        <button className="sort-elem-btn">
+          <span className="material-icons"> filter_list </span>
+        </button>
+        <button className="remove-all-btn">
+          <span className="material-icons"> delete_sweep </span>
+        </button>
+      </div>
     </div>
   );
 }
